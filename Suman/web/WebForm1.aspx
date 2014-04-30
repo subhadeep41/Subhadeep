4503b0b51bc143f5ff3c0ad8b8b5cb2687fab7d8
Subhadeep41 <subhadeep41@gmail.com>
2014-05-01 02:00:49 +0530

    changes

---
+++ Suman/web/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="web.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h4>Select a file to upload:</h4>
+            <asp:FileUpload ID="FileUpload1" runat="server"></asp:FileUpload>
+            <br />
+            <br />
+            <asp:Button ID="ShowButton" Text="Show output" OnClick="ShowButton_Click" runat="server"></asp:Button>
+
+            <asp:Label ID="Label1" style="display:none" runat="server" Text="Label"></asp:Label>
+
+        </div>
+        <div id="gridid">
+            <asp:GridView ID="grdViewCustomers" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Suman/web/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void ShowButton_Click(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Name");
+            table.Columns.Add("Address");
+
+            using (StreamReader sr = new StreamReader(@"C:\Users\test.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] parts = sr.ReadLine().Split(':');
+                    table.Rows.Add(parts[0], parts[1]);
+                }
+            }
+            grdViewCustomers.DataSource = table;
+            grdViewCustomers.DataBind();
+        }
+    }
+}
